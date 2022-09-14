7edeac74e3900fdf75afd5c61952cc5fe52bd7ad
Ibnhu <113009188+Ibnhu@users.noreply.github.com>
2022-09-14 14:11:41 +0700

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,32 @@
-<1DOCTYPE html>
+<1DOCTYPE html > 
+< html >
+< head > 
+< title > Login Form < / title >
+< link rel = " stylesheet " href = " style.css " type = " text / css " > 
+< / head > 
+< body >
+
+ < l-- < h2 > Login Form < / h2 > -- > 
+< form action = " tampil.php " method = " POST " > 
+< h2 > Form Login < / h2 > 
+< div class = " imgcontainer " > 
+< img src = " icon. png " alt = " Avatar " class = " avatar " >
+ < / div > 
+
+< div class = " container " > 
+< label > < b > Username < / b > < / label > 
+< input type = " text " placeholder " Masukkan Username " name = " username " required >
+name = " email " required >
+
+ < label > < b > Fassword < / b > < / label > 
+< input type = " password " placeholder = " Masukkan Password " < button type = " submit " > Masuk < / button > < input type = " checkbox " checked = " checked " > < span > Ingat Saya </span>
+< / div >
+
+ < div class = " container " style = " background - color : # f1f1f1 " > 
+< button type = " button " class = " cancelbtn " > Batal < / but ton > 
+< span class = " psw " > Lupa <a href="#"> password ? </a> < / span >
+< / div >
+< / form > 
+
+< / body > 
+< / html >
